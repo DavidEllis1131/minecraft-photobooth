00f83c084d90bbd34faf2e9a8de10146d0d0f534
DavidEllis1131 <daveey2811daveey2811@gmail.com>
2015-09-10 14:05:58 +0100

    Death Machinery V1.0
    Made a death machine

--- code/photobooth.py
+++ code/photobooth.py
@@ -8,7 +8,7 @@ def take_the_pic():
     with picamera.PiCamera() as camera:
         camera.start_preview()
         time.sleep(2)
-        camera.capture('selfie.jpg')
+        camera.capture('Final words.jpg')
 
 def where_am_i():
     while True:
@@ -17,14 +17,14 @@ def where_am_i():
         time.sleep(3)
         
         if x >= 10.5 and y == 9.0 and z == -44.3:
-        #print "You are at the photobooth!"
+        #print "You are at the photobooth! Type /kill un chat!"
         
-            mc.postToChat("You are in the Photobooth!")
+            mc.postToChat("You are in the Photobooth! Type /kill un chat!")
             time.sleep(1)
-            mc.postToChat("Smile!")
+            mc.postToChat("Die!!!!!")
             time.sleep(1)
             take_the_pic()
-            mc.postToChat("Check out your picture")
+            mc.postToChat("Check out your picture OR ELSE!!!!")
             time.sleep(5)
         
 mc.postToChat("Find the Photo-Booth")
